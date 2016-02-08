0f73202ded7a64324144db1a2ff50c33a8fe9a06
mauromsl <mauro.slopezs@gmail.com>
2016-02-08 15:46:21 -0800

    initial commit

---
+++ python_src/default_settings.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+
+DATABASE={
+	'user':'root',
+	'password':'',
+	'host':'127.0.0.1',
+	'database':'', 
+}
+
+
+try:
+  connection = connector.connect(**DATABASE)
+except connector.Error as error:
+  if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif error.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(error)
+else:
+  connection.close()
