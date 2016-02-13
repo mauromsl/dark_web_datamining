0bd12a9f9614dd28958ca6b4a2c7ce818170a223
Mauro Sanchez <mauro.slopezs@gmail.com>
2016-02-13 18:35:07 +0000

    database patch: add USD and GBP columns to tblProduct

--- python_src/db.py
+++ python_src/db.py
@@ -1,28 +1,44 @@
 #!/usr/bin/env python
-''' Controls actions on the database '''
+""" Controls actions on the database """
 
 import settings
-import db_patches
+from db_patches import db_patches_list
 
-def validate_patches():
-	#TODO: check patches applied and 
-
-def validate_query(query_string, safe_mode):
-	''' if safe safe mode is enabled, queries that alter the schema won't be allowed'''
-	verbs_not_allowed = ['create, alter, grant']
-	if safe_mode:
-		if any(verb in query_string.lower() for verb in verbs_not_allowed):
-			return False
-
-	return True
 
 def query(query_string, safe_mode=True):
-	valid_query = validate_query(query_string, safe)
-	if valid_query
-		cursor = settings.connection.cursor()
-		return cursor
-	else:
-		raise Exception('SQL verbs that alter schema are not allowed in safe mode')
+    """ Given a query string returns a cursor object """
+    valid_query = validate_query(query_string, safe_mode)
+    if valid_query:
+        cursor = settings.connection.cursor()
+        return cursor.execute(query_string, multi=True)
+    else:
+        raise Exception(
+            'SQL verbs that alter schema are not allowed in safe mode')
 
 
-		
+def validate_query(query_string, safe_mode):
+    """On safe mode, reject schema alterations """
+    verbs_not_allowed = ['create, alter, grant']
+    if safe_mode:
+        if any(verb in query_string.lower() for verb in verbs_not_allowed):
+            return False
+
+    return True
+
+
+def validate_patches():
+    """ Ensure that the required db patches have been applied
+    and applies those requireds
+    """
+    for patch in db_patches_list():
+        print ("##### validating patch %s ####" % (patch.name))
+        print (patch.validation)
+        is_valid = query(patch.validation, safe_mode=False)
+        if is_valid:
+            print ('Validation OK')
+        else:
+            print ('Not valid - Applying patch ...')
+            query(patch.sql, safe_mode=False)
+
+
+validate_patches()
--- python_src/db_patches.py
+++ python_src/db_patches.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
-''' contains all the patches that have been applied to the database in order to keep schemas consistent across environments '''
+"""
+contains all the patches that have been applied to
+the database in order to keep schemas consistent across environments
+"""
 
 
-PATCHES = []
+class DBPatch():
+    def __init__(self, name, sql, validation, *args, **kwargs):
+        self.name = name
+        self.sql = sql
+        self.validation = validation
 
+patches = []
+
+sql = """
+    ALTER TABLE `dark_web`.`tblProduct`
+    ADD COLUMN `USD` DECIMAL(15,2) UNSIGNED NULL COMMENT '' AFTER `product_picture`,
+    ADD COLUMN `GBP` DECIMAL(15,2) UNSIGNED NULL COMMENT '' AFTER `USD`;
+    """
+
+validation = """
+    SELECT *
+    FROM information_schema.COLUMNS
+    WHERE
+        TABLE_SCHEMA = 'dark_web'
+        AND TABLE_NAME = 'tblProducts'
+        AND COLUMN_NAME = 'GBP';
+        """
+additional_currencies_patch = DBPatch(
+    name='additional_currencies_patch',
+    sql=sql,
+    validation=validation
+)
+
+patches.append(additional_currencies_patch)
+
+
+def db_patches_list():
+    """ Returns a list containing all the patches required by the project """
+    return patches
