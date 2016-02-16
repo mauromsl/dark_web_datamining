6fe0420297999ea0e8b44041a445945f4d76f38c
Mauro Sanchez <mauro.slopezs@gmail.com>
2016-02-16 21:06:55 +0000

    adds function call to bitcoin api

---
+++ python_src/logic.py
@@ -0,0 +1,16 @@
+import settings
+import requests
+
+
+def get_bitcoin_conversions(currency, start_date, end_date):
+    """
+    Given a currency code, start date and end date,
+    returns dict with daily bitcoin conversion
+    """
+    api_url = settings.BITCOIN_API.format(
+        start_date=start_date,
+        end_date=end_date,
+        currency=currency
+        )
+    request = requests.get(api_url)
+    return request.json()
